ef7dddd4bc4fa60091fca219ccffd010b22713d3
jonas <Jonas_Falk.Jakobsen@Student.Reutlingen-University.DE>
2017-06-18 21:53:59 +0200

    dummy API working locally

---
+++ app/routes.js
@@ -0,0 +1,106 @@
+var path = require('path');
+var sanitizer = require('sanitizer');
+
+var Artist = require('./Artist');
+
+var count = 0;
+var clients = {};
+
+module.exports = function (app) {
+
+  //READ ALL ARTISTS (GET)
+  app.get('/api/artists', function(req, res){
+    console.log("first HERE GET");
+  var nameparameter = req.query.name;
+  var nameparametersanitized = sanitizer.escape(nameparameter);
+    Artist.find({'name' : new RegExp(nameparametersanitized, 'i')}, function(err, users) {
+      if (err) throw err;
+
+      res.json(users);
+    });
+  });
+
+  //READ SPECIFIC ARTISTS (GET)
+  app.get('/api/artist', function(req, res){
+    console.log("first HERE GET");
+  var nameparameter = req.query.name;
+  // console.log(nameparameter);
+  var nameparametersanitized = sanitizer.escape(nameparameter);
+    Artist.find({'name' : new RegExp(nameparametersanitized, 'i')}, function(err, users) {
+      if (err) throw err;
+
+      res.json(users);
+    });
+  });
+
+  //INSERT NEW ARTIST (POST)
+  app.post('/api/artist', function(req, res){
+console.log("first HERE POST");
+    res.send(req.body);
+
+    //sanitizing
+    var sanitizename = sanitizer.escape(req.body.name);
+    var sanitizebplace = sanitizer.escape(req.body.birthPlace);
+
+    var newArtist = new Artist({
+      name: sanitizename,
+      birthPlace: sanitizebplace,
+      birthDate: req.body.birthDate,
+      favoritebool: req.body.favoritebool
+    });
+
+    //Mongoose Save Function to save data
+    newArtist.save(function(error, product, numAffected) {
+      
+      if (error) {
+        console.error(error);
+      }
+      if (numAffected == 1) {
+        for(var i in clients){
+            // Send a message to the client with the message
+            clients[i].sendUTF("datasend");
+        }
+      }
+
+    });
+
+  });
+
+  //UPDATE ARTIST (PUT)
+  app.put('/api/artist', function(req, res){
+
+    res.send(req.body);
+    var artistid = req.body.selectedid;
+
+    Artist.update({'id': artistid}, {
+        favoritebool: req.body.afavorite
+    }, function(err, numberAffected, rawResponse) {
+       //handle it
+       for(var i in clients){
+           // Send a message to the client with the message
+           clients[i].sendUTF("datasend");
+       }
+    })
+
+  });
+
+  //DELETE ARTIST
+  app.delete('/api/artist', function(req, res){
+
+    res.send(req.body);
+    var delid = req.body.selectedid;
+
+     //Mongoose Save Funtktion to save data
+    Artist.findOneAndRemove({id : delid}, function(error) {
+      if (error) {
+        console.error(error);
+      }
+      for(var i in clients){
+          // Send a message to the client with the message
+          clients[i].sendUTF("datasend");
+      }
+    });
+    
+  });
+
+};
---
+++ main.js
@@ -0,0 +1,57 @@
+/** 
+    MODULES
+**/
+var WebSocketServer = require('websocket').server;
+//express for middleware(static files), POST/GET methods
+var express = require('express')
+var app = express()
+//body parser for encoding and getting POST parameters (and maybe URL's)
+var bodyParser = require('body-parser')
+//path for static files (core module)
+var path = require('path');
+//http (core module)
+var http = require('http');
+//mongoose makes it easier to communicate with mongodb
+var mongoose = require('mongoose');
+
+//artist class
+var connectdb = require('./config/db.js');
+var routes = require('./app/routes.js');
+
+//connect to mongodb
+connectdb();
+
+/**
+    USED MIDDLEWARE
+**/
+
+//static files
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//body parser
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+//Create and Start a server
+//Must be at the and, first we create our handle functions and than we start the server
+const PORT= process.env.PORT || 8080;
+var server = http.createServer(app);
+server.listen(PORT, function(){
+  console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+//ENTRY-POINT (INDEX-PAGE)
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+ 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed. 
+  return true;
+}
+
+//routes
+routes(app);
+
