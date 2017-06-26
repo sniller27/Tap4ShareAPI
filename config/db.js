a117b156647695b3dde7d7f8354aec51124b52b3
jonas <Jonas_Falk.Jakobsen@Student.Reutlingen-University.DE>
2017-06-27 01:39:00 +0200

    ignore added images + change mongo connection string

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ var mongoose = require('mongoose');
 
 module.exports = function () { 
 
-	var URLmongodb = 'mongodb://john:1234@ds131512.mlab.com:31512/tap4share';
-	// mongodb://<dbuser>:<dbpassword>@ds131512.mlab.com:31512/tap4share
+	var env = process.env.NODE_ENV || 'dev';
+	console.log(env);
+	// var URLmongodb = 'mongodb://john:1234@ds131512.mlab.com:31512/tap4share';
+	
+	var URLmongodb = 'mongodb://localhost:27017/tap4share';
 
 	//Mongoose Connection
 	mongoose.connect(URLmongodb); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
